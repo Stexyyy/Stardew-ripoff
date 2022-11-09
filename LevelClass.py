220a1367b2a1305e937b70490ed7e2e00531a3dc
Stexyyy <78552805+Stexyyy@users.noreply.github.com>
2022-11-09 08:19:45 -0700

    Update LevelClass.py

--- LevelClass.py
+++ LevelClass.py
@@ -2,5 +2,17 @@ import pygame
 from settings import * #imports everything from the settings file
 
 class Level:
+    def __init__(self):
+        
+        #gets a reference (address) to the currently set display surface
+        self.display_surface - pygame.display.get_surface()
+        
+        #class "group" is part of pygame's sprite support. it is a class that manages a *list* of sprites.
+        self.all_sprites = pygame.sprite.Group()
+        
+        
     def run(self, dt):
         print("run game")
+        self.display_surface.fill('black')
+        self.all_sprites.draw(self.display_surface) #a draw function from the group class
+        self.all_sprites.update() #another function from the group class
