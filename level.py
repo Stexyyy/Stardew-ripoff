e3c4d0e050ea6c8ccbf5d30aac72e5d4c47eb981
Stexyyy <78552805+Stexyyy@users.noreply.github.com>
2022-11-09 09:39:14 -0700

    Update level.py

--- level.py
+++ level.py
@@ -1,24 +1,63 @@
 import pygame
-from settings import * #imports everything from the settings file
-from player import Player
+from settings import *
+from support import *
 
-class Level:
-    def __init__(self):
+class Player(pygame.sprite.Sprite): #this is a child class of pygame's sprite class
+    def __init__(self, pos, group):
+        super().__init__(group) #this gives this class access to the functions inside the group class
         
-        #gets a reference (address) to the currently set display surface
-        self.display_surface = pygame.display.get_surface()
+        self.import_assets()
         
-        #class "group" is part of pygame's sprite support. it is a class that manages a *list* of sprites.
-        self.all_sprites = pygame.sprite.Group()
+        #general setup
+        self.image = pygame.Surface((32,64))
+        self.image.fill('green')
+        self.rect = self.image.get_rect(center = pos) #set the position to be the center of the rect
         
-        self.setup() #call the setup function
+        #movement attributes
+        self.direction = pygame.math.Vector2()
+        self.pos = pygame.math.Vector2(self.rect.center) #player x, y position
+        self.speed = 200
     
-    def setup(self):
-        self.player = Player((640,360), self.all_sprites) #this gets handed a position and the sprites list as parameters
+    def import_assets(self):
+        self.animations = {'up':[], 'down':[], 'left':[], 'right':[],
+                           'right_idle':[], 'left_idle':[], 'up_idle':[], 'down_idle':[],
+                           'right_hoe':[], 'left_hoe':[], 'up_hoe':[], 'down_hoe':[],
+                           'right_axe':[], 'left_axe':[], 'up_axe':[], 'down_axe':[],
+                           'right_water':[], 'left_water':[], 'up_water':[], 'down_water':[]}
+        
+        for animation in self.animations.keys():
+            full_path = '../graphics/character/' + animation
+            self.animations[animation]=import_folder(full_path)
+        
+    def input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+            
+        if keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        elif keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
+            
+        #print(self.direction)
+            
+    def move(self, dt):
+        if self.direction.magnitude() > 0: #can't normalize vectors with length of 0
+            self.direction = self.direction.normalize() #normalize the vector
+        #print(self.direction) uncomment when testing
+        #horizontal movement
+        self.pos.x += self.direction.x * self.speed * dt
+        self.rect.centerx = self.pos.x
+        #vertical movement
+        self.pos.y += self.direction.y * self.speed * dt
+        self.rect.centery = self.pos.y
     
-    
-    def run(self, dt):
-        #print("run game")
-        self.display_surface.fill('black')
-        self.all_sprites.draw(self.display_surface) #a draw function from the group class
-        self.all_sprites.update(dt) #another function from the group class
+    def update(self,dt):
+        self.input()
+        self.move(dt)
