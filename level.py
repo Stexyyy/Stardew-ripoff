821d6ec8fd8d0440265bb51a790162b67364d210
Stexyyy <78552805+Stexyyy@users.noreply.github.com>
2022-11-09 08:26:51 -0700

    Update and rename LevelClass.py to level.py

--- level.py
+++ level.py
@@ -1,5 +1,6 @@
 import pygame
 from settings import * #imports everything from the settings file
+from player import Player
 
 class Level:
     def __init__(self):
@@ -10,9 +11,14 @@ class Level:
         #class "group" is part of pygame's sprite support. it is a class that manages a *list* of sprites.
         self.all_sprites = pygame.sprite.Group()
         
-        
+        self.setup() #call the setup function
+    
+    def setup(self):
+        self.player = Player((640,360), self.all_sprites) #this gets handed a position and the sprites list as parameters
+    
+    
     def run(self, dt):
         print("run game")
         self.display_surface.fill('black')
         self.all_sprites.draw(self.display_surface) #a draw function from the group class
-        self.all_sprites.update() #another function from the group class
+        self.all_sprites.update(dt) #another function from the group class
